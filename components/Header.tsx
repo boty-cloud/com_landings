777f8de0e979542dfb9c0709df5c0a51ed1c524c
Nico Donnelly <nicodonnelly@MacBook-Pro-de-Nico.local>
2026-06-25 18:04:43 -0300

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,16 +14,20 @@ export default function Header({ botyLogo, clientLogo, clientLogoSecondary, clie
         {botyLogo && (
           <>
             <Image src={botyLogo} alt="Boty" width={160} height={40} className="header__logo" />
-            <span className="header__sep">&amp;</span>
+            {botyLogo && clientLogo && (<span className="header__sep">&amp;</span>)}
           </>
         )}
-        <Image
-          src={clientLogo}
-          alt={clientName}
-          width={176}
-          height={44}
-          className="header__logo header__logo--client"
-        />
+        {clientLogo && (
+          <>
+            <Image
+              src={clientLogo}
+              alt={clientName}
+              width={176}
+              height={44}
+              className="header__logo header__logo--client"
+            />
+          </>
+        )}
         {clientLogoSecondary && (
           <>
             <span className="header__sep">&amp;</span>
